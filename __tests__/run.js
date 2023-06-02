4329ba36be9d4f868525936469ea68e843ba8dc2
timeo.perrinjaquet@gmail.com <timeo.perrinjaquet@gmail.com>
2023-06-02 17:23:13 +0200

    WIP

--- __tests__/run.js
+++ __tests__/run.js
@@ -2,7 +2,7 @@ const { Redstone } = require('../src/Redstone')
 const { RedstoneBlock } = require('../src/RedstoneBlock')
 const { Repeater } = require('../src/Repeater')
 
-const { run_list } = require('../src/utils')
+const { run_list, tab_link } = require('../src/utils')
 
 show = (component) => {
     if(component instanceof Redstone) { return 'Redstone : ' + component.intensity }
@@ -10,7 +10,9 @@ show = (component) => {
     if(component instanceof Repeater) { return 'Repeater : ' + component.intensity }
 }
 
-let rd = [
+// List of components
+
+/*let rd = [
     new RedstoneBlock(),
     new Redstone(),
     new Redstone(),
@@ -42,4 +44,23 @@ run_list(rd)
 
 rd.forEach(element => {
     console.log(show(element))
-});
+});*/
+
+// 2D array of components
+
+let tab = [
+	[null, null, new Redstone(), null, null],
+	[null, new Redstone(), new Redstone(), new Redstone(), null],
+	[null, null, new RedstoneBlock(), null, null],
+]
+
+tab_link(tab)
+
+tab.forEach(row => {
+	row.forEach(element => {
+		if(element != null) {
+			console.log(show(element))
+			console.log(element.links)
+		}
+	})
+})
--- src/utils.js
+++ src/utils.js
@@ -8,10 +8,31 @@ list_link = (list) => {
     list[0].spread(null)
 }
 
-tab_link = (tab, start_index=[]) => {
-    if(start_index === []) {
-        tab.find(row => ![null,undefined].includes(row[0]))
+
+first_non_null_element = (arr) => {
+    for(let i = 0; i < arr.length; i++) {
+        for(let j = 0; j < arr[i].length; j++) {
+            if(arr[i][j] != null) { return [i, j] }
+        }
     }
+}
+
+adjacent_cells = (arr, x, y) => {
+    let adjacents = []
+    for(const co of [[x + 1, y],[x - 1, y],[x, y + 1],[x, y - 1]]) {
+        if(co[0] <= arr.length - 1 && co[1] <= arr.length - 1 && co[0] >= 0 && co[1] >= 0) { adjacents.push([co[0], co[1]])}
+    }
+	return adjacents
+}
+
+tab_link = (tab, start_index=[]) => {
+	done = []
+    start_index = start_index.length == 0 ? first_non_null_element(tab) : start_index
+	for(const co of adjacent_cells(tab, start_index[0], start_index[1])) {
+		console.log('[tab_link-st] :', tab[start_index[0]][start_index[1]])
+		console.log('[tab_link-co] :', tab[co[0]][co[1]])
+		if(tab[co[0]][co[1]] != null) { tab[start_index[0]][start_index[1]].link(tab[co[0]][co[1]]) }
+	}
 }
 
 find_update_needed_component = (first_component, before=null) => {
